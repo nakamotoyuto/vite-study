02eb301c1e0b518f2f6064b17eeb521653b2cec2
仲本勇斗 <yutonakamoto03@gmail.com>
2021-03-19 23:57:56 +0900

    add ts file

---
+++ src/@use/useController.ts
@@ -0,0 +1,9 @@
+import { reactive } from 'vue';
+import { useStore } from 'vuex'
+
+export const useController = () => {
+  const store = useStore()
+  const state = reactive<{ todoList: Array<{ id: number, value: string }> }>({
+    todoList: store.state.todoList
+  });
+}
